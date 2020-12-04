c47c5d3fa0b2d69b2b54c17f274877d72f171f81
Fernandocgomez <fernandocgomez@live.com>
2020-12-03 23:51:59 -0600

    Bootstrap, debuger, and components have been added to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { BookContainerComponent } from './components/book-container/book-container.component';
+import { BookComponent } from './components/book/book.component';
+import { ReviewComponent } from './components/review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignUpComponent,
+    BookContainerComponent,
+    BookComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
